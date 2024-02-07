690e5a868d52848ef0e09c744999838a7bd1fc90
Veru Satharasingha <Veru Satharasingha@DESKTOP-BN34K29>
2024-02-07 13:42:42 +0530

    Add project files.

---
+++ Lab sheet-02-question 01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab_sheet_02_question_01
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book();
+            myBook.Title = "Harry Potter and the Philosopher's Stone";
+            myBook.Author = "J.K. Rowling";
+
+            Console.WriteLine("Title: " + myBook.Title);
+            Console.WriteLine("Author: " + myBook.Author);
+        }
+    }
+}
